d7995a616c305ec36f2a78252731d3395114803a
Inderpal Virdi <inderpal.virdi@ext.sidekickhealth.com>
2026-06-18 11:05:30 +0100

    Fix middleware blocking API route

--- middleware.ts
+++ middleware.ts
@@ -16,5 +16,5 @@ export function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|assets|fonts).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|assets|fonts|api).*)"],
 };
